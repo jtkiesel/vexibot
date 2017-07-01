98ac40b2f2742e09015d354bd245aeea0978ff6c
jtkiesel <rstudio@example.com>
2017-07-01 03:16:33 -0400

    Debugging.

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const Discord = require('discord.js');
 const sql = require('sqlite');
 
-var commandNames = ['ping', 'uptime', 'update', 'team', 'awards'];
+const client = new Discord.Client();
+const token = process.env.DISCORD_TOKEN;
+const prefix = '^';
+const commandNames = ['ping', 'uptime', 'update', 'team', 'awards'];
+
 var commands = {};
 for (var name of commandNames) {
 	commands[name] = require('./commands/' + name);
 }
-/*
-var cmdPing = require('./commands/ping');
-var cmdUptime = require('./commands/uptime');
-var cmdUpdate = require('./commands/update');
-var cmdTeam = require('./commands/team');
-var cmdAwards = require('./commands/awards');
-*/
-const client = new Discord.Client();
-const token = process.env.DISCORD_TOKEN;
-const prefix = '^';
 
 sql.open('./vexdb.sqlite');
 
@@ -34,30 +28,9 @@ client.login(token);
 var handleCommand = message => {
 	var [cmd, args] = message.content.substring(prefix.length).split(' ', 2);
 
-	commands[cmd](message, args);
-/*
-	switch (cmd) {
-		case 'ping':
-			cmdPing(message, args);
-			break;
-
-		case 'uptime':
-			cmdUptime(message, args);
-			break;
-
-		case 'update':
-			cmdUpdate(message, args);
-
-		case 'team':
-			cmdTeam(message, args);
-			break;
-
-		case 'awards':
-			cmdAwards(message, args);
-
-		default:
-			message.reply('Unrecognized command.');
-			break;
+	if (commandNames.includes(cmd)) {
+		commands[cmd](message, args);
+	} else {
+		message.reply('Unrecognized command.');
 	}
-*/
 }
--- commands/update.js
+++ commands/update.js
@@ -1,2 +1,48 @@
+const Discord = require('discord.js');
+const http = require('http');
+const sql = require('sqlite');
+
+const resources = ['events', 'teams', 'matches', 'rankings', 'season_rankings', 'awards', 'skills'];
+
 module.exports = (message, args) => {
+	var embed = new Discord.RichEmbed()
+			.setColor('RANDOM')
+			.setDescription('Updating...');
+	message.channel.send({embed});
+
+	addResourceBatchToTable(0, 0, Date.now());
+};
+
+var addResourceBatchToTable = (resourceIndex, startIndex, startTime) => {
+	var body = '';
+	http.request({
+		host: 'api.vexdb.io',
+		path: '/v1/get_' + resources[resourceIndex] + '?apikey=shNhxcphXlIXQVE2Npeu&limit_start=' + startIndex
+	}, response => {
+		response.on('data', chunk => {
+			body += chunk;
+		});
+		response.on('end', () => {
+			body = JSON.parse(he.decode(body));
+
+			if (body.status == 1) {
+				if (body.size > 0) {
+					for (var element of body.result) {
+						var row = '';
+						for (var attribute in element) {
+							if (row != '') {
+								row += ','
+							}
+							row += attribute;
+						}
+						console.log(row);
+					}
+				} else {
+					console.log('done');
+				}
+			} else {
+				message.reply('Sorry, VexDB messed up.');
+			}
+		});
+	}).end();
 };
