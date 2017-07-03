7fed181a8a56e4ff9f8822305fc8e1f844021612
jtkiesel <rstudio@example.com>
2017-07-02 22:36:15 -0400

    Debugging.

--- update.js
+++ update.js
@@ -6,12 +6,12 @@ const db = require('sqlite');
 const tablesToColumns = require('./dbinfo');
 
 db.open('./vexdb.sqlite')
-		.then(() => Object.entries(tablesToColumns).forEach((table, columns) => {
+		.then(() => Object.entries(tablesToColumns).forEach([table, columns] => {
 			var columnDefinitions = [];
-			Object.entries(columns).forEach((column, type) => {
+			Object.entries(columns).forEach([column, type] => {
 				columnDefinitions.push(`${column} ${type}`);
 			});
-			console.log(columnDefinitions);
+			console.log(columns);
 			console.log(columnDefinitions.join(', '));
 			console.log(`CREATE TABLE IF NOT EXISTS ${table} (${columnDefinitions.join(', ')})`);
 			db.run(`CREATE TABLE IF NOT EXISTS ${table} (${columnDefinitions.join(', ')})`)
