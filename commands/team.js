7e19758302a110dd6b29d889346afbfffd81b1df
jtkiesel <rstudio@example.com>
2017-07-05 01:10:00 -0400

    Debugging.

--- commands/team.js
+++ commands/team.js
@@ -2,8 +2,10 @@ const Discord = require('discord.js');
 const db = require('sqlite');
 const he = require('he');
 
+const vex = require('../vex');
+
 module.exports = (message, args) => {
-	var teamId;
+	/*var teamId;
 	if (args) {
 		teamId = args.trim().toUpperCase();
 	} else {
@@ -12,35 +14,39 @@ module.exports = (message, args) => {
 	if (/^([0-9]{1,5}[A-Z]?|[A-Z]{2,6}[0-9]{0,2})$/.test(teamId)) {
 		db.get(`SELECT * FROM teams WHERE number = ?`, teamId)
 			.then(team => {
-				if (team) {
-					var team_name = he.decode(team.team_name);
-					var robot_name = he.decode(team.robot_name);
-					var organisation = he.decode(team.organisation);
-					var location = [team.city];
-					if (team.region && team.region !== 'N/A' && team.region !== 'Not Applicable or Not Listed') {
-						location.push(team.region);
-					}
-					if (team.country) {
-						location.push(team.country);
-					}
-					location = location.join(', ');
+				if (team) {*/
+	vex.getTeam(message, args).then(team => {
+		if (team) {
+			var team_name = he.decode(team.team_name);
+			var robot_name = he.decode(team.robot_name);
+			var organisation = he.decode(team.organisation);
+			var location = [team.city];
+			if (team.region && team.region !== 'N/A' && team.region !== 'Not Applicable or Not Listed') {
+				location.push(team.region);
+			}
+			if (team.country) {
+				location.push(team.country);
+			}
+			location = location.join(', ');
 
-					var embed = new Discord.RichEmbed()
-						.setColor('AQUA')
-						.setTitle(team.number)
-						.setURL(`https://vexdb.io/teams/view/${team.number}`)
-						.addField('Team Name', team_name, true);
+			var embed = new Discord.RichEmbed()
+				.setColor('AQUA')
+				.setTitle(team.number)
+				.setURL(`https://vexdb.io/teams/view/${team.number}`)
+				.addField('Team Name', team_name, true);
 
-					if (robot_name) {
-						embed.addField('Robot Name', robot_name, true);
-					}
-					if (organisation) {
-						embed.addField('Organization', organisation, true);
-					}
-					embed.addField('Location', location, true);
+			if (robot_name) {
+				embed.addField('Robot Name', robot_name, true);
+			}
+			if (organisation) {
+				embed.addField('Organization', organisation, true);
+			}
+			embed.addField('Location', location, true);
 
-					message.channel.send({embed});
-				} else {
+			message.channel.send({embed});
+		}
+	}).catch(console.error);
+				/*} else {
 					message.reply('That team ID has never been registered.');
 				}
 			}).catch(error => {
@@ -49,5 +55,5 @@ module.exports = (message, args) => {
 			});
 	} else {
 		message.reply('Please provide a valid team ID.');
-	}
+	}*/
 };
