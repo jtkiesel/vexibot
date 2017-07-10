8ba0df47d17aa56f194717b1964783aba9950ef6
jtkiesel <rstudio@example.com>
2017-07-10 13:09:04 -0400

    Changed ping calculation to be closer to what is actually desired.

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 
 module.exports = (message, args) => {
+	const time = Date.now();
 	const embed = new Discord.RichEmbed()
 		.setColor('RANDOM')
 		.setDescription('🏓 Pong!');
 
 	message.channel.send({embed}).then(reply => {
-		embed.setDescription(`${embed.description} \`${reply.client.ping / 1000}s\``);
+		embed.setDescription(`${embed.description} \`${(Date.now() - time) / 1000}s\``);
 		reply.edit({embed});
 	}).catch(console.error);
 };
