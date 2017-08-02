9adbddcb088473c3c9e909dc796948a051f8f6fc
jtkiesel <rstudio@example.com>
2017-08-02 02:16:55 -0400

    Fixed topskills command.

--- commands/topskills.js
+++ commands/topskills.js
@@ -5,6 +5,7 @@ const dbinfo = require('../dbinfo');
 const vex = require('../vex');
 
 const db = app.db;
+const grades = dbinfo.grades;
 
 const rankEmojis = ['🥇', '🥈', '🥉'];
 const defaultEmoji = '🏅';
@@ -17,17 +18,17 @@ module.exports = (message, args) => {
 	let limit;
 	if (grade == 'HS') {
 		program = 'VRC';
-		grade = 'High School';
+		grade = grades.indexOf('High School');
 		season = 119;
 		limit = 35;
 	} else if (grade == 'MS') {
 		program = 'VRC';
-		grade = 'Middle School';
+		grade = grades.indexOf('Middle School');
 		season = 119;
 		limit = 15;
 	} else if (grade == 'C' || grade == 'U') {
 		program = 'VEXU';
-		grade = 'College';
+		grade = grades.indexOf('College');
 		season = 120;
 		limit = 5;
 	} else {
@@ -41,21 +42,21 @@ module.exports = (message, args) => {
 		if (teams.length) {
 			description = '';
 			for (let i = 0; i < teams.length; i++) {
-				const rank = (i < 3) ? `${rankEmojis[i]} \`:` : `\`#${String(i + 1).padEnd(2)} :`;
-				const score = String(teams[i].scores.score).padStart(3);
+				const rank = (i < 3) ? `${rankEmojis[i]}` : `**\`#${String(i + 1).padEnd(2)}\​\`**`;
+				const score = String(teams[i].score).padStart(3);
 				const team = teams[i].team.id;
-				description += `${rank} ${score}\`   [${team}](https://vexdb.io/teams/view/${team}?t=skills)\n`;
+				description += `${rank}   \`\​${score}\`   [${team}](https://vexdb.io/teams/view/${team}?t=skills)\n`;
 			}
 			const embed = new Discord.RichEmbed()
 				.setColor('AQUA')
-				.setTitle(`${program} ${grade} In the Zone Robot Skills`)
+				.setTitle(`${program} ${grades[grade]} In the Zone Robot Skills`)
 				.setURL(`https://vexdb.io/skills/${program}/${seasonName}/Robot`)
 				.setDescription(description);
 			message.channel.send({embed})
 				.then(reply => app.addFooter(message, embed, reply))
 				.catch(console.error);
 		} else {
-			message.reply(`no skills scores available for ${program} ${grade} In the Zone.`);
+			message.reply(`no skills scores available for ${program} ${grades[grade]} In the Zone.`);
 		}
 	}).catch(console.error);
 };
