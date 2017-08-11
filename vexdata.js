3f60430425a8e5c49d35ac521c9a84bb323e7a5d
jtkiesel <rstudio@example.com>
2017-08-11 00:15:40 -0400

    Updating all teams from Robot Events.

--- vexdata.js
+++ vexdata.js
@@ -18,8 +18,8 @@ const updateAwards = () => updateCollectionFromResource('awards', 'get_awards', 
 const updateSkills = () => updateCollectionFromResource('skills', 'get_skills', formatSkill);
 
 const updateReTeams = () => {
-	updateTeamsForSeason(1, 73);
-	updateTeamsForSeason(4, 76);
+	updateTeamsForSeason(1, 85);
+	updateTeamsForSeason(4, 88);
 	/*[{_id: 1, seasons: [119, 115, 110, 102, 92, 85, 73, 7, 1]},
 		{_id: 4, seasons: [120, 116, 111, 103, 93, 88, 76, 10, 4]}].forEach(program => {
 		const seasons = program.seasons.sort((a, b) => a - b);
