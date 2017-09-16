bd69dd341c0b17b866a8cd5bad3048101e8fb250
jtkiesel <rstudio@example.com>
2017-09-16 15:05:03 -0400

    Fixed bug checking if match is 0-0 or not yet scored. Was checking if scored, but then saying it wasn't.

--- events.js
+++ events.js
@@ -310,7 +310,8 @@ const updateEvent = async (prog, sku, retried = false) => {
 				let scored = true;
 				if (match.redScore === 0 && match.blueScore === 0) {
 					if (match._id.round < 3) {  // Practice or qualification.
-						if (matches.slice(i + 1).some(otherMatch => otherMatch.redScore !== 0 || otherMatch.blueScore !== 0)) {
+						if (matches.slice(i + 1).every(otherMatch => otherMatch.redScore == 0 && otherMatch.blueScore == 0)) {
+							console.log(matches.slice(i + 1));
 							scored = false;
 						}
 					/*} else if (match._id.round === 2) {  // Qualification.
