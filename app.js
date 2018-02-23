67ed9515c264ca1a46047bdb9ca5eace6b19fe42
Jordan Taylor Kiesel <jtkiesel@users.noreply.github.com>
2018-02-23 15:20:27 -0500

    Reverting last commit.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const mongodb = require('mongodb');
 const util = require('util');
-const events = require('events');
 
 const client = new Discord.Client();
 const MongoClient = mongodb.MongoClient;
